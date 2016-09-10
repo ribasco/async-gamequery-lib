7448dd001c7c6f6d27b7828f95b71fbc4b844837
Rafael Ibasco <ribasco@gmail.com>
2016-09-10 21:44:03 +0800

    Fixed import errors

--- src/main/java/com/ribasco/gamecrawler/clients/SourceClient.java
+++ src/main/java/com/ribasco/gamecrawler/clients/SourceClient.java
@@ -1,7 +1,7 @@
-/*
+/***************************************************************************************************
  * MIT License
  *
- * Copyright (c) [year] [fullname]
+ * Copyright (c) 2016 Rafael Ibasco
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */
+ **************************************************************************************************/
 
 package com.ribasco.gamecrawler.clients;
 
@@ -30,9 +30,9 @@ import com.ribasco.gamecrawler.protocols.handlers.ErrorHandler;
 import com.ribasco.gamecrawler.protocols.valve.server.SourceMasterFilter;
 import com.ribasco.gamecrawler.protocols.valve.server.SourcePlayer;
 import com.ribasco.gamecrawler.protocols.valve.server.SourceServer;
+import com.ribasco.gamecrawler.protocols.valve.server.handlers.SourcePacketHandler;
 import com.ribasco.gamecrawler.protocols.valve.server.handlers.SourceRequestEncoder;
 import com.ribasco.gamecrawler.protocols.valve.server.handlers.SourceResponseDecoder;
-import com.ribasco.gamecrawler.protocols.valve.server.handlers.SourcePacketHandler;
 import com.ribasco.gamecrawler.protocols.valve.server.handlers.SourceResponseHandler;
 import com.ribasco.gamecrawler.protocols.valve.server.packets.requests.*;
 import io.netty.channel.ChannelPipeline;

bf93b0ef22bcbe0f7fa7063bb15a527105241849
Rafael Luis Ibasco <raffy@ibasco.com>
2016-09-10 21:44:03 +0800

    Fixed import errors

--- src/test/java/com/ribasco/gamecrawler/protocols/valve/server/SourceMasterFilterTest.java
+++ src/test/java/com/ribasco/gamecrawler/protocols/valve/server/SourceMasterFilterTest.java
@@ -1,4 +1,28 @@
-package com.ribasco.gamecrawler.protocols.valve.source;
+/***************************************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2016 Rafael Ibasco
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **************************************************************************************************/
+
+package com.ribasco.gamecrawler.protocols.valve.server;
 
 import com.ribasco.gamecrawler.protocols.valve.server.SourceMasterFilter;
 import org.junit.After;

18fcbebebbb93bfd11aa4d1acffa914d4e7d5d04
Rafael Ibasco <ribasco@gmail.com>
2016-11-07 00:48:42 +0800

    Restored missing files in steam master package

---
+++ protocols/valve/steam/master/src/main/java/org/ribasco/agql/protocols/valve/steam/master/MasterServerRequest.java
@@ -0,0 +1,53 @@
+/***************************************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2016 Rafael Luis Ibasco
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **************************************************************************************************/
+
+package org.ribasco.agql.protocols.valve.steam.master;
+
+import org.ribasco.agql.core.AbstractGameServerRequest;
+import org.ribasco.agql.protocols.valve.steam.master.enums.MasterServerRegion;
+import org.ribasco.agql.protocols.valve.steam.master.packets.MasterServerRequestPacket;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by raffy on 9/15/2016.
+ */
+public class MasterServerRequest<T> extends AbstractGameServerRequest<MasterServerRequestPacket> {
+
+    private MasterServerRegion region;
+    private MasterServerFilter filter;
+    private InetSocketAddress startIp;
+
+    public MasterServerRequest(InetSocketAddress recipient, MasterServerRegion region, MasterServerFilter filter, InetSocketAddress startIp) {
+        super(recipient);
+        this.region = region;
+        this.filter = filter;
+        this.startIp = startIp;
+    }
+
+    @Override
+    public MasterServerRequestPacket getMessage() {
+        return new MasterServerRequestPacket(region, filter, startIp);
+    }
+}

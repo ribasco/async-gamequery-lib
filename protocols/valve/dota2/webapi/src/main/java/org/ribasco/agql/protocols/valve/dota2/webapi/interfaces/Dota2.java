0f87b10d590fd6347aa9f0e349d5bc933cb7e44f
Rafael Luis Ibasco <raffy@ibasco.com>
2016-11-07 00:54:33 +0800

    Restored missing files in dota 2 webapi package

---
+++ protocols/valve/dota2/webapi/src/main/java/org/ribasco/agql/protocols/valve/dota2/webapi/interfaces/Dota2.java
@@ -0,0 +1,17 @@
+package org.ribasco.agql.protocols.valve.dota2.webapi.interfaces;
+
+import org.ribasco.agql.protocols.valve.steam.webapi.SteamWebApiClient;
+
+public final class Dota2 {
+    public static Dota2Econ createEcon(SteamWebApiClient client) {
+        return new Dota2Econ(client);
+    }
+
+    public static Dota2Fantasy createFantasy(SteamWebApiClient client) {
+        return new Dota2Fantasy(client);
+    }
+
+    public static Dota2Match createMatch(SteamWebApiClient client) {
+        return new Dota2Match(client);
+    }
+}

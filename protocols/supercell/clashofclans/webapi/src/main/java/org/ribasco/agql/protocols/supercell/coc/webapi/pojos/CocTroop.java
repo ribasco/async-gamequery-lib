7e5391047c614cdbb59de663ea36bce270d3d591
Rafael Ibasco <ribasco@gmail.com>
2016-11-07 00:36:38 +0800

    Restored missing files in coc webapi package

---
+++ protocols/supercell/clashofclans/webapi/src/main/java/org/ribasco/agql/protocols/supercell/coc/webapi/pojos/CocTroop.java
@@ -0,0 +1,43 @@
+package org.ribasco.agql.protocols.supercell.coc.webapi.pojos;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class CocTroop {
+    private String name;
+    private int level;
+    private int maxLevel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getMaxLevel() {
+        return maxLevel;
+    }
+
+    public void setMaxLevel(int maxLevel) {
+        this.maxLevel = maxLevel;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE)
+                .append("name", getName())
+                .append("level", getLevel())
+                .append("maxlevel", getMaxLevel())
+                .toString();
+    }
+}

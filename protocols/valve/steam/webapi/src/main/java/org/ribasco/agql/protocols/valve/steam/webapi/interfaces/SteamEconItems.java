338d5d8d653ec707a6fcd51ab208ff9855207f1f
Rafael Ibasco <ribasco@gmail.com>
2016-11-07 21:34:46 +0800

    Updated copyright header for all project related files

--- protocols/valve/steam/webapi/src/main/java/org/ribasco/agql/protocols/valve/steam/webapi/interfaces/SteamEconItems.java
+++ protocols/valve/steam/webapi/src/main/java/org/ribasco/agql/protocols/valve/steam/webapi/interfaces/SteamEconItems.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Asynchronous Game Query Library
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.ribasco.agql.protocols.valve.steam.webapi.interfaces;
 
 import com.google.gson.JsonArray;
@@ -79,10 +103,7 @@ public class SteamEconItems extends SteamWebApiInterface {
 
     public CompletableFuture<SteamEconItemsStoreMeta> getStoreMetadata(int appId, int version) {
         CompletableFuture<JsonObject> json = sendRequest(new GetStoreMetadata(appId, version));
-        return json.thenApply(root -> {
-            JsonObject result = root.getAsJsonObject("result");
-            return fromJson(result, SteamEconItemsStoreMeta.class);
-        });
+        return json.thenApply(root -> fromJson(getSteamResult(root), SteamEconItemsStoreMeta.class));
     }
 
     public CompletableFuture<Integer> getStoreStatus(int appId, int version) {

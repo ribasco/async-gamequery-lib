bfd004b368aefb484ee20c98bb5f01ecc8156e6e
Rafael Ibasco <ribasco@gmail.com>
2016-11-25 06:15:18 +0800

    Updated travis config file. Added site build scripts

---
+++ scripts/deploy-site.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    ./build-site.sh
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+ls -l

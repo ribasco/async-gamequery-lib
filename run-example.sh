1e669d7748ea1c3cb412dc18ff00236d14f0c35d
Rafael Ibasco <ribasco@gmail.com>
2016-11-28 18:52:48 +0800

    Added check for missing arguments

--- run-example.sh
+++ run-example.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e # Exit with nonzero exit code if anything fails
 
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Please specify the key representing the example. (e.g. source-query)"
+fi
+
 echo "Running example for ${1}"
 
 mvn -f examples/pom.xml exec:java -Dexec.args="-e ${1}"

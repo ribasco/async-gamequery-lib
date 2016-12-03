5fcfa5adc10cf1ab6039a760215226709b27bc67
Rafael Ibasco <ribasco@gmail.com>
2016-12-04 02:00:30 +0800

    Updated travis config files

--- scripts/deploy-project.sh
+++ scripts/deploy-project.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
+set -ev # Exit with nonzero exit code if anything fails
 
 echo "============================================="
 echo "Deploying Project to OSS Sonatype"
 echo "============================================="
 
 # Do not deploy on pull-requests or commits on other branches
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
-    echo "Skipping deploy. Performing project-build only."
+if [ "${TRAVIS_PULL_REQUEST}" != "false" -o "${TRAVIS_BRANCH}" != "master" ]; then
+    echo "Skipping deploy."
     scripts/build-project.sh
     exit 0
 fi

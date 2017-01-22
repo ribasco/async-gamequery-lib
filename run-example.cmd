ebfaf76b6f8c69a79f64b8550ec6fc721c9f78e6
Rafael Ibasco <ribasco@gmail.com>
2017-01-23 02:39:22 +0800

    [#7] Added example runner batch script for Windows

---
+++ run-example.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+if "%1"=="" goto usage
+
+echo Running example '%1'
+
+mvn -f examples/pom.xml exec:java -Dexec.args="-e %1"
+
+goto done
+
+:usage
+    echo Error: Missing Example Key. Please specify the example key. (e.g. source-query)
+    echo
+    echo ====================================================================
+    echo List of available examples"
+    echo ====================================================================
+    echo - Source Server Query Example      (key: source-query)
+    echo - Master Server Query Example      (key: master-query)
+    echo - Source Rcon Example              (key: source-rcon)
+    echo - Clash of Clans Web API Example   (key: coc-webapi)
+    echo - CS:GO Web API Example            (key: csgo-webapi)
+    echo - Steam Web API Example            (key: steam-webapi)
+    echo - Steam Storefront Web API Example (key: steam-store-webapi)
+    echo - Source Log Listener Example      (key: source-log)
+    echo - Steam Econ Web API Example       (key: steam-econ-webapi)
+    echo - Minecraft Rcon Example           (key: mc-rcon)
+
+:done

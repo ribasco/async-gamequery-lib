216ec2e6fc5efba6d4894ae35df9dcd44a1eb5a4
Rafael Luis Ibasco <ribasco@gmail.com>
2022-01-27 19:19:05 +0800

    Remove unused methods
    Signed-off-by: Rafael Luis Ibasco <ribasco@gmail.com>

--- core/src/main/java/com/ibasco/agql/core/transport/BootstrapNettyChannelFactory.java
+++ core/src/main/java/com/ibasco/agql/core/transport/BootstrapNettyChannelFactory.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022 Asynchronous Game Query Library
+ * Copyright (c) 2022 Asynchronous Game Query Library
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -157,21 +157,9 @@ abstract public class BootstrapNettyChannelFactory implements NettyChannelFactor
     @Override
     abstract public CompletableFuture<Channel> create(Envelope<? extends AbstractRequest> envelope);
 
-    public final ChannelHandler getChannelInitializer() {
-        return channelInitializer;
-    }
-
-    public final NettyChannelHandlerInitializer getChannelHandlerInitializer() {
-        return channelHandlerInitializer;
-    }
-
     @Override
     public EventLoopGroup getExecutor() {
         return eventLoopGroup;
-    }
-
-    public final TransportType getTransportType() {
-        return transportType;
     }
 
     public final Bootstrap getBootstrap() {

bb999d5838715a3c8a3ca037398b4716d93d2c70
Rafael Luis Ibasco <ribasco@gmail.com>
2022-01-27 11:23:24 +0800

    Updated javadocs
    Signed-off-by: Rafael Luis Ibasco <ribasco@gmail.com>

--- core/src/main/java/com/ibasco/agql/core/transport/pool/NettyChannelPool.java
+++ core/src/main/java/com/ibasco/agql/core/transport/pool/NettyChannelPool.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2021-2022 Asynchronous Game Query Library
+ * Copyright (c) 2022 Asynchronous Game Query Library
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@ package com.ibasco.agql.core.transport.pool;
 
 import com.ibasco.agql.core.AbstractRequest;
 import com.ibasco.agql.core.Envelope;
+import static com.ibasco.agql.core.util.Functions.TRUE;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOutboundInvoker;
 import io.netty.util.AttributeKey;
@@ -44,17 +45,35 @@ public interface NettyChannelPool extends Closeable {
 
     ReleaseStrategy DISCONNECT_ON_RELEASE = ChannelOutboundInvoker::disconnect;
 
+    /**
+     * The default {@link Channel} attribute for {@link NettyChannelPool}
+     */
     AttributeKey<NettyChannelPool> CHANNEL_POOL = AttributeKey.newInstance(NettyChannelPool.class.getName());
 
     /**
-     * Acquire a {@link Channel} from this {@link NettyChannelPool}. The returned {@link Future} is notified once
+     * Acquire a {@link Channel} from this {@link NettyChannelPool}. The returned {@link CompletableFuture} is notified once
      * the acquire is successful and failed otherwise.
      *
      * <strong>Its important that an acquired is always released to the pool again, even if the {@link Channel}
      * is explicitly closed..</strong>
+     *
+     * @param envelope
+     *         The request envelope that will be used to acquire a {@link Channel}
      */
     CompletableFuture<Channel> acquire(Envelope<? extends AbstractRequest> envelope);
 
+    /**
+     * Acquire a {@link Channel} from this {@link NettyChannelPool}. The returned {@link CompletableFuture} is notified once
+     * the acquire is successful and failed otherwise.
+     *
+     * <strong>Its important that an acquired is always released to the pool again, even if the {@link Channel}
+     * is explicitly closed..</strong>
+     *
+     * @param envelope
+     *         The request envelope that will be used to acquire a {@link Channel}
+     * @param promise
+     *         The {@link CompletableFuture} that is notified once the acquire operation is complete.
+     */
     CompletableFuture<Channel> acquire(Envelope<? extends AbstractRequest> envelope, final CompletableFuture<Channel> promise);
 
     /**
@@ -69,17 +88,48 @@ public interface NettyChannelPool extends Closeable {
      */
     CompletableFuture<Void> release(Channel channel, CompletableFuture<Void> promise);
 
-    @Override
-    void close();
+    /**
+     * Attempts to release the {@link Channel} if it is pooleed.
+     *
+     * @param channel
+     *         The {@link Channel} to release
+     *
+     * @return A {@link CompletableFuture} which is notified once the operation is marked as completed. A value of {@code true} will be returned if the {@link Channel} was released, otherwise {@code false} if the {@link Channel} is not pooled or if the release operation was unsuccessful. The returned future will never complete exceptionally.
+     */
+    static CompletableFuture<Boolean> tryRelease(Channel channel) {
+        NettyChannelPool pool = getPool(channel);
+        if (pool == null)
+            return CompletableFuture.completedFuture(false);
+        return pool.release(channel).thenApply(TRUE).exceptionally(e -> false);
+    }
 
+    /**
+     * Retrieve the {@link NettyChannelPool} which was used to acquire the specified {@link Channel}
+     *
+     * @param channel
+     *         The {@link Channel} to be used as lookup
+     *
+     * @return The {@link NettyChannelPool} or {@code null} if the {@link Channel} was not acquired from a {@link NettyChannelPool}
+     */
     static NettyChannelPool getPool(Channel channel) {
         if (channel == null || !channel.hasAttr(CHANNEL_POOL))
             return null;
         return channel.attr(CHANNEL_POOL).get();
     }
 
+    /**
+     * Checks if the {@link Channel} is currectly pooled
+     *
+     * @param channel
+     *         The {@link Channel} to be used as lookup
+     *
+     * @return {@code true} if the {@link Channel} has been acquired from a {@link NettyChannelPool}
+     */
     static boolean isPooled(Channel channel) {
         Objects.requireNonNull(channel, "Channel must not be null");
         return getPool(channel) != null;
     }
+
+    @Override
+    void close();
 }

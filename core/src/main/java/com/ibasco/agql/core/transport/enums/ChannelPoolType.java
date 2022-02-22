4a310488bde90e15da5962087cd376ad244d8573
Rafael Luis Ibasco <ribasco@gmail.com>
2022-02-22 15:55:28 +0800

    Updated javadoc
    Signed-off-by: Rafael Luis Ibasco <ribasco@gmail.com>

--- core/src/main/java/com/ibasco/agql/core/transport/enums/ChannelPoolType.java
+++ core/src/main/java/com/ibasco/agql/core/transport/enums/ChannelPoolType.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2021-2022 Asynchronous Game Query Library
+ * Copyright (c) 2022 Asynchronous Game Query Library
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,15 +17,15 @@
 package com.ibasco.agql.core.transport.enums;
 
 /**
- * The type of {@link io.netty.channel.pool.ChannelPool} implementation to use.
+ * The type of {@link com.ibasco.agql.core.transport.pool.NettyChannelPool} implementation to use.
  *
  * @author Rafael Luis Ibasco
  */
 public enum ChannelPoolType {
     /**
-     * An unbounded channel pool implementation. Connections will be made on-demand without limitation.
+     * An unbounded channel pool implementation. Connections will be made on-demand without limitation. Connection will be released once the request has been completed.
      *
-     * @see io.netty.channel.pool.SimpleChannelPool
+     * @see com.ibasco.agql.core.transport.pool.SimpleNettyChannelPool
      * @see com.ibasco.agql.core.transport.pool.NettyChannelPool
      */
     ADAPTIVE,
@@ -34,7 +34,7 @@ public enum ChannelPoolType {
      * A bounded channel pool implementation. A fixed number of connections will be used. If the requests surpass
      * the maximum number of connections allowed, then the requests will be queued until a connection is made available.
      *
-     * @see io.netty.channel.pool.FixedChannelPool
+     * @see com.ibasco.agql.core.transport.pool.FixedNettyChannelPool
      * @see com.ibasco.agql.core.transport.pool.NettyChannelPool
      */
     FIXED

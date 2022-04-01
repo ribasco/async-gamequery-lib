77f553d993339293d7a4f2fcb4d5e51dc2d91db5
Rafael Luis Ibasco <ribasco@gmail.com>
2022-04-01 13:26:42 +0800

    Include payload data in exception
    Signed-off-by: Rafael Luis Ibasco <ribasco@gmail.com>

--- core/src/main/java/com/ibasco/agql/core/exceptions/InvalidPacketException.java
+++ core/src/main/java/com/ibasco/agql/core/exceptions/InvalidPacketException.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2018-2022 Asynchronous Game Query Library
+ * Copyright (c) 2022 Asynchronous Game Query Library
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,23 +17,15 @@
 package com.ibasco.agql.core.exceptions;
 
 public class InvalidPacketException extends ResponseProcessingException {
-    public InvalidPacketException() {
-        super();
-    }
 
-    public InvalidPacketException(String message) {
+    private final byte[] data;
+
+    public InvalidPacketException(String message, byte[] data) {
         super(message);
+        this.data = data;
     }
 
-    public InvalidPacketException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidPacketException(Throwable cause) {
-        super(cause);
-    }
-
-    public InvalidPacketException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public byte[] getData() {
+        return data;
     }
 }
